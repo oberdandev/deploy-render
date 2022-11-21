91a1ecc05a25b850df5f263feab417479603d24b
unknown <Admin@OE-NUMOA-DADOS.semsa.am.br>
2022-11-21 13:46:23 -0400

    added error page html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use((req,res) => {
-    res.send('meu primeiro deploy!')
-})
+const requestHandle = (req,res) => {
+   if(req.url == '/'){
+    res.send('oi')
+    return
+   }
+   else
+   {
+    res.sendFile(path.resolve('./src/error.html'))
+   }
+}
+
+app.use(requestHandle)
 
 app.listen(PORT, ()=> {
     console.log('app is running at port ', PORT)
