6cfa3d46ad8dd60be0e42357cdbec62f76d54cc5
unknown <Admin@OE-NUMOA-DADOS.semsa.am.br>
2022-11-21 12:52:21 -0400

    Project setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.use((req,res) => {
+    res.send('meu primeiro deploy!')
+})
+
+app.listen(PORT, ()=> {
+    console.log('app is running at port ', PORT)
+})
+
